164def388757e1722a31437893c1b0ce1d87d5a0
Adarsh Kumar Pandey <fakp24@gmail.com>
2023-11-29 13:05:43 +0530

    now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useMemo} from 'react'
+import styled from "styled-components";
+const bg =require('./img/bg1.jpg')
+import {MainLayout} from './styles/Layouts'
+import Orb from './components/Orb/Orb'
+import Navigation from "./components/Navigation/Navigation";
+import Dashboard from './components/Dashboard/Dashboard'
+import Income from './components/Income/Income'
+import Expenses from './components/Expenses/Expenses'
+import { useGlobalContext } from './context/globalContext';
 
 function App() {
+  const [active, setActive] = useState(1)
+  const orbMemo = useMemo(() => {
+    return <Orb />
+  },[])
+  
+  const displayData = () => {
+    switch(active){
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Income />
+      case 4: 
+        return <Expenses />
+      default: 
+        return <Dashboard />
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStyled bg={bg} className="App">
+      {orbMemo}
+    <MainLayout>
+    <Navigation active={active} setActive={setActive} />
+    <main>
+          {displayData()}
+    </main>
+    </MainLayout>
+        
+    </AppStyled>
   );
 }
+
+const AppStyled=styled.div`
+height: 100vh;
+background-image: url(${props => props.bg});
+position: relative;
+main{
+  flex: 1;
+  background: rgba(252, 246, 249, 0.78);
+  border: 3px solid #FFFFFF;
+  backdrop-filter: blur(4.5px);
+  border-radius: 32px;
+  overflow-x: hidden;
+  &::-webkit-scrollbar{
+    width: 0;
+  }
+}
+
+
+`;
 
 export default App;
